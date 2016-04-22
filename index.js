0ced44df133aa9ac065fa9df244d7f0e9e87bc54
Timothy Bauman <tebauman@gmail.com>
2016-04-22 17:21:48 +0000

    edited server, fixed json file

--- index.js
+++ index.js
@@ -7,24 +7,28 @@ var app = express();
 app.use('/', express.static(__dirname + '/public'));
 app.get('/:input', function(req, res){
     var input = new Date(req.params.input);
-    
     var date = {
         unix: null,
         natural: null
     }
     
     if(input == 'Invalid Date'){
+        input = new Date(parseInt(req.params.input));
+    }
+    
+    if(input == 'Invalid Date'){
         res.json(date);
     }
     else{
-        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var month = months[input.getMonth()];
-        date.natural = month + ' ' + input.getDate() + ', ' + input.getFullYear();
+    
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = months[input.getMonth()];
+    date.natural = month + ' ' + input.getDate() + ', ' + input.getFullYear();
         
-        date.unix = input.getTime();
-        
-        res.json(date);
+    date.unix = input.getTime();
+    res.json(date);
     }
+    
 })
 
 var port = process.env.PORT || 5000;
