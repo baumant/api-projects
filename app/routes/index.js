d23799dacb9e21ebd3c29816cd0d33a1a8986c1d
Timothy Bauman <tebauman@gmail.com>
2016-04-25 16:43:53 -0700

    added CORS headers

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,11 @@ var timestamp = require(process.cwd() + '/app/controllers/timestamp.js');
 var whoami = require(process.cwd() + '/app/controllers/whoami.js');
 
 module.exports = function(app) {
-
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     app.route('/')
         .get(function(req, res) {
             res.sendFile(process.cwd() + '/public/index.html');
